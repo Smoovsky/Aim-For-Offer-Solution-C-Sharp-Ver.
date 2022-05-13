08dc2db2b80303f39e57aabc76d30e76efa68349
Shaun Chen <shang.chen@globirdenergy.com.au>
2022-05-13 22:52:55 +0800

    1. continued

---
+++ Chapter2/P100CountOneShiftOne.cs
@@ -0,0 +1,46 @@
+﻿namespace Algorithm
+{
+    public static partial class Solution
+    {
+        public static int P100CountOneShiftOne(int num)
+        {
+            uint i = 1;
+
+            var result = 0;
+
+            while (i != 0)
+            {
+                if ((num & i) != 0)
+                {
+                    result++;
+                }
+
+                i <<= 1;
+            }
+
+            return result;
+        }
+
+        public static int P100CountOneNoShift(int num)
+        {
+            var result = 0;
+
+            while (num != 0)
+            {
+                num &= num - 1;
+                result++;
+            }
+
+            return result;
+        }
+
+        public static void TestP100CountOne()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"{i}:{P100CountOneShiftOne(i)}");
+                Console.WriteLine($"{i}:{P100CountOneNoShift(i)}");
+            }
+        }
+    }
+}
