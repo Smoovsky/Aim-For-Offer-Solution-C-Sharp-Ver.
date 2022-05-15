ae7f34eb8d80fdd1120d0cb18c103bb29760b993
Shaun Chen <shang.chen@globirdenergy.com.au>
2022-05-15 21:48:35 +0800

    1. continued

---
+++ Chapter2/P116PrintNumberByDecimalCount.cs
@@ -0,0 +1,100 @@
+﻿using System.Linq;
+
+namespace Algorithm
+{
+    public static partial class Solution
+    {
+        public static void P116PrintNumberByDecimalCount(int length)
+        {
+            static void P116PrintNumberByDecimalCountRecursive(
+            char[] str,
+            int length,
+            int index)
+            {
+                if (index == length)
+                {
+                    P116PrintNum(str);
+
+                    return;
+                }
+
+                for (byte i = 0; i < 10; i++)
+                {
+                    str[index] = (char)('0' + i);
+
+                    P116PrintNumberByDecimalCountRecursive(str, length, index + 1);
+                }
+            }
+
+            P116PrintNumberByDecimalCountRecursive(
+                new char[length],
+                length,
+                0);
+        }
+
+        public static void P116PrintNumberByDecimalCountNonRecursive(int length)
+        {
+            var str = Enumerable.Range(0, length).Select(_ => '0').ToArray();
+
+            static bool Incremet(char[] str)
+            {
+                var overflow = false;
+
+                var carry = 0;
+                var sum = 0;
+
+                for (int i = str.Length - 1; i >= 0; i--)
+                {
+                    sum = str[i] - '0' + carry;
+
+                    if (i == str.Length - 1)
+                    {
+                        sum++;
+                    }
+
+                    if (sum >= 10)
+                    {
+                        if (i == 0)
+                        {
+                            overflow = true;
+                        }
+                        else
+                        {
+                            sum -= 10;
+                            carry = 1;
+                            str[i] = (char)('0' + sum);
+                        }
+                    }
+                    else
+                    {
+                        str[i] = (char)('0' + sum);
+                        break;
+                    }
+                }
+
+                if (overflow)
+                {
+                    return false;
+                }
+
+                return true;
+            };
+
+            while (Incremet(str))
+            {
+                P116PrintNum(str);
+            }
+        }
+
+        public static void P116PrintNum(char[] str)
+        {
+            Console.WriteLine(new string(str).TrimStart('0'));
+        }
+
+        public static void TestP116PrintNumberByDecimalCount()
+        {
+            P116PrintNumberByDecimalCount(5);
+            P116PrintNumberByDecimalCountNonRecursive(5);
+        }
+    }
+}
---
+++ Chapter2/P120DelLinkedListNode.cs
@@ -0,0 +1,40 @@
+﻿namespace Algorithm
+{
+    public static partial class Solution
+    {
+        public class P120LinkedListNode<T>
+        {
+            public T Value { get; set; }
+
+            public P120LinkedListNode<T> Next { get; set; }
+        }
+
+        public static void P120DelLinkedListNode<T>(
+            ref P120LinkedListNode<T> head,
+            P120LinkedListNode<T> nodeToDel)
+        {
+            if (nodeToDel.Next != null)
+            {
+                nodeToDel.Value = nodeToDel.Next.Value;
+                nodeToDel.Next = nodeToDel.Next;
+            }
+            else if (head == nodeToDel) // del first node
+            {
+                head = null;
+            }
+            else // del last node
+            {
+                var prev = head;
+                var current = head.Next;
+
+                while (current.Next != null)
+                {
+                    prev = current;
+                    current = current.Next;
+                }
+
+                prev.Next = null;
+            }
+        }
+    }
+}
