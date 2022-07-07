86e422f68a343cd91bdaf475e19357ace818d9c7
Shaun Chen <shang.chen@globirdenergy.com.au>
2022-07-07 22:30:20 +0800

    1. continued

---
+++ Chapter4/P183TreePathSum.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithm.Chapter4;
+
+public static partial class Solution
+{
+    public static void P183TreePathSum(
+        TreeNode<int>? current,
+        Stack<int>? path,
+        int expected)
+    {
+        if (current == null)
+        {
+            return;
+        }
+
+        path ??= new();
+        path.Push(current.Value);
+
+        if (current.Left == null
+            && current.Right == null
+            && path.Sum() == expected)
+        {
+            Console.WriteLine($"Path found: {string.Join(", ", path.Select(i => i.ToString()))}");
+        }
+
+        if (current.Left != null)
+        {
+            P183TreePathSum(current.Left, path, expected);
+        }
+
+        if (current.Right != null)
+        {
+            P183TreePathSum(current.Right, path, expected);
+        }
+
+        path.Pop();
+    }
+
+    // ====================测试代码====================
+    public static void P183Test(string P183TestName, TreeNode<int>? pRoot, int expectedSum)
+    {
+        if (P183TestName != null)
+            Console.Write($"{P183TestName} begins:\n");
+
+        P183TreePathSum(pRoot, null, expectedSum);
+
+        Console.Write("\n");
+    }
+
+    //            10
+    //         /      \
+    //        5        12
+    //       /\        
+    //      4  7     
+    // 有两条路径上的结点和为22
+    public static void P183Test1()
+    {
+        TreeNode<int>? pNode10 = new(10);
+        TreeNode<int>? pNode5 = new(5);
+        TreeNode<int>? pNode12 = new(12);
+        TreeNode<int>? pNode4 = new(4);
+        TreeNode<int>? pNode7 = new(7);
+
+        TreeNode<int>.ConnectTreeNodes(pNode10, pNode5, pNode12);
+        TreeNode<int>.ConnectTreeNodes(pNode5, pNode4, pNode7);
+
+        Console.Write("Two paths should be found in P183Test1.\n");
+        P183Test("P183Test1", pNode10, 22);
+
+
+    }
+
+    //            10
+    //         /      \
+    //        5        12
+    //       /\        
+    //      4  7     
+    // 没有路径上的结点和为15
+    public static void P183Test2()
+    {
+        TreeNode<int>? pNode10 = new(10);
+        TreeNode<int>? pNode5 = new(5);
+        TreeNode<int>? pNode12 = new(12);
+        TreeNode<int>? pNode4 = new(4);
+        TreeNode<int>? pNode7 = new(7);
+
+        TreeNode<int>.ConnectTreeNodes(pNode10, pNode5, pNode12);
+        TreeNode<int>.ConnectTreeNodes(pNode5, pNode4, pNode7);
+
+        Console.Write("No paths should be found in P183Test2.\n");
+        P183Test("P183Test2", pNode10, 15);
+
+
+    }
+
+    //               5
+    //              /
+    //             4
+    //            /
+    //           3
+    //          /
+    //         2
+    //        /
+    //       1
+    // 有一条路径上面的结点和为15
+    public static void P183Test3()
+    {
+        TreeNode<int>? pNode5 = new(5);
+        TreeNode<int>? pNode4 = new(4);
+        TreeNode<int>? pNode3 = new(3);
+        TreeNode<int>? pNode2 = new(2);
+        TreeNode<int>? pNode1 = new(1);
+
+        TreeNode<int>.ConnectTreeNodes(pNode5, pNode4, null);
+        TreeNode<int>.ConnectTreeNodes(pNode4, pNode3, null);
+        TreeNode<int>.ConnectTreeNodes(pNode3, pNode2, null);
+        TreeNode<int>.ConnectTreeNodes(pNode2, pNode1, null);
+
+        Console.Write("One path should be found in P183Test3.\n");
+        P183Test("P183Test3", pNode5, 15);
+
+
+    }
+
+    // 1
+    //  \
+    //   2
+    //    \
+    //     3
+    //      \
+    //       4
+    //        \
+    //         5
+    // 没有路径上面的结点和为16
+    public static void P183Test4()
+    {
+        TreeNode<int>? pNode1 = new(1);
+        TreeNode<int>? pNode2 = new(2);
+        TreeNode<int>? pNode3 = new(3);
+        TreeNode<int>? pNode4 = new(4);
+        TreeNode<int>? pNode5 = new(5);
+
+        TreeNode<int>.ConnectTreeNodes(pNode1, null, pNode2);
+        TreeNode<int>.ConnectTreeNodes(pNode2, null, pNode3);
+        TreeNode<int>.ConnectTreeNodes(pNode3, null, pNode4);
+        TreeNode<int>.ConnectTreeNodes(pNode4, null, pNode5);
+
+        Console.Write("No paths should be found in P183Test4.\n");
+        P183Test("P183Test4", pNode1, 16);
+
+
+    }
+
+    // 树中只有1个结点
+    public static void P183Test5()
+    {
+        TreeNode<int>? pNode1 = new(1);
+
+        Console.Write("One path should be found in P183Test5.\n");
+        P183Test("P183Test5", pNode1, 1);
+
+
+    }
+
+    // 树中没有结点
+    public static void P183Test6()
+    {
+        Console.Write("No paths should be found in P183Test6.\n");
+        P183Test("P183Test6", null, 0);
+    }
+}
---
+++ Chapter4/P187CopyComplexList.cs
@@ -0,0 +1,220 @@
+﻿namespace Algorithm.Chapter4;
+
+public static partial class Solution
+{
+    public class ComplexListNode
+    {
+        public int Value { get; set; }
+
+        public ComplexListNode Next { get; set; }
+
+        public ComplexListNode Other { get; set; }
+
+        public ComplexListNode(int val) => Value = val;
+
+        public ComplexListNode() { }
+
+        public ComplexListNode Copy() => new()
+        {
+            Value = Value,
+            Next = Next,
+            Other = Other
+        };
+
+        public static void PrintList(ComplexListNode pHead)
+        {
+            ComplexListNode pNode = pHead;
+
+            while (pNode != null)
+            {
+                Console.Write($"The value of this node is: {pNode.Value}.\n");
+
+                if (pNode.Other != null)
+                    Console.Write($"The value of its sibling is: {pNode.Other.Value}.\n");
+                else
+                    Console.Write("This node does not have a sibling.\n");
+
+                Console.Write("\n");
+
+                pNode = pNode.Next;
+            }
+        }
+
+        public static void BuildNodes(
+            ComplexListNode pNode,
+            ComplexListNode pNext,
+            ComplexListNode pSibling)
+        {
+            if (pNode != null)
+            {
+                pNode.Next = pNext;
+                pNode.Other = pSibling;
+            }
+        }
+    }
+
+    public static ComplexListNode P187CopyComplexList(
+        ComplexListNode source)
+    {
+        if (source == null)
+        {
+            return null;
+        }
+
+        var current = source;
+
+        while (current != null)
+        {
+            var temp = current.Next;
+
+            current.Next = current.Copy();
+            current.Next.Next = temp;
+
+            current = temp;
+        }
+
+        current = source;
+        int i = 0;
+
+        while (current != null)
+        {
+            if ((i & 1) == 1
+                && current.Other != null)
+            {
+                current.Other = current.Other.Next;
+            }
+
+            current = current.Next;
+
+            i++;
+        }
+
+        var original = source;
+        var result = source.Next;
+
+        var originalIteration = original;
+        var resultIteration = result;
+
+        current = source;
+        i = 0;
+
+        while (current != null)
+        {
+            if (i > 1)
+            {
+                if ((i & 1) == 0)
+                {
+                    originalIteration.Next = current;
+                    originalIteration = originalIteration.Next;
+                }
+                else
+                {
+                    resultIteration.Next = current;
+                    resultIteration = resultIteration.Next;
+                }
+            }
+
+            current = current.Next;
+            i++;
+        }
+
+        return result;
+    }
+
+    //          -----------------
+    //         \|/              |
+    //  1-------2-------3-------4-------5
+    //  |       |      /|\             /|\
+    //  --------+--------               |
+    //          -------------------------
+    public static void P187Test1()
+    {
+        ComplexListNode pNode1 = new(1);
+        ComplexListNode pNode2 = new(2);
+        ComplexListNode pNode3 = new(3);
+        ComplexListNode pNode4 = new(4);
+        ComplexListNode pNode5 = new(5);
+
+        ComplexListNode.BuildNodes(pNode1, pNode2, pNode3);
+        ComplexListNode.BuildNodes(pNode2, pNode3, pNode5);
+        ComplexListNode.BuildNodes(pNode3, pNode4, null);
+        ComplexListNode.BuildNodes(pNode4, pNode5, pNode2);
+
+        P187Test("P187Test1", pNode1);
+    }
+
+    // m_pSibling指向结点自身
+    //          -----------------
+    //         \|/              |
+    //  1-------2-------3-------4-------5
+    //         |       | /|\           /|\
+    //         |       | --             |
+    //         |------------------------|
+    public static void P187Test2()
+    {
+        ComplexListNode pNode1 = new(1);
+        ComplexListNode pNode2 = new(2);
+        ComplexListNode pNode3 = new(3);
+        ComplexListNode pNode4 = new(4);
+        ComplexListNode pNode5 = new(5);
+
+        ComplexListNode.BuildNodes(pNode1, pNode2, null);
+        ComplexListNode.BuildNodes(pNode2, pNode3, pNode5);
+        ComplexListNode.BuildNodes(pNode3, pNode4, pNode3);
+        ComplexListNode.BuildNodes(pNode4, pNode5, pNode2);
+
+        P187Test("P187Test2", pNode1);
+    }
+
+    // m_pSibling形成环
+    //          -----------------
+    //         \|/              |
+    //  1-------2-------3-------4-------5
+    //          |              /|\
+    //          |               |
+    //          |---------------|
+    public static void P187Test3()
+    {
+        ComplexListNode pNode1 = new(1);
+        ComplexListNode pNode2 = new(2);
+        ComplexListNode pNode3 = new(3);
+        ComplexListNode pNode4 = new(4);
+        ComplexListNode pNode5 = new(5);
+
+        ComplexListNode.BuildNodes(pNode1, pNode2, null);
+        ComplexListNode.BuildNodes(pNode2, pNode3, pNode4);
+        ComplexListNode.BuildNodes(pNode3, pNode4, null);
+        ComplexListNode.BuildNodes(pNode4, pNode5, pNode2);
+
+        P187Test("P187Test3", pNode1);
+    }
+
+    // 只有一个结点
+    public static void P187Test4()
+    {
+        ComplexListNode pNode1 = new(1);
+        ComplexListNode.BuildNodes(pNode1, null, pNode1);
+
+        P187Test("P187Test4", pNode1);
+    }
+
+    // 鲁棒性测试
+    public static void P187Test5()
+    {
+        P187Test("P187Test5", null);
+    }
+
+    public static void P187Test(string testName, ComplexListNode pHead)
+    {
+        if (testName != null)
+            Console.Write($"{testName} begins:\n");
+
+        Console.Write("The original list is:\n");
+        ComplexListNode.PrintList(pHead);
+
+        ComplexListNode pClonedHead = P187CopyComplexList(pHead);
+
+        Console.Write("The cloned list is:\n");
+        ComplexListNode.PrintList(pClonedHead);
+    }
+}
