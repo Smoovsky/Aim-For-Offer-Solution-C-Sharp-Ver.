345086c0ebb2fb24177b297ef603a2b80c65042f
Shaun Chen <shang.chen@globirdenergy.com.au>
2022-08-18 21:12:26 +0800

    1. continued

--- Chapter5/P244NonDupChar.cs
+++ Chapter5/P244NonDupChar.cs
@@ -27,6 +27,66 @@ public static partial class Solution
         return '\0';
     }
 
+    public static void Test(string pString, char expected)
+    {
+        if (P244NonDupChar(pString) == expected)
+            Console.Write("Test passed.\n");
+        else
+            Console.Write("Test failed.\n");
+    }
+
+    public static void P224Test01()
+    {
+        // 常规输入测试，存在只出现一次的字符
+        Test("google", 'l');
+
+        // 常规输入测试，不存在只出现一次的字符
+        Test("aabccdbd", '\0');
+
+        // 常规输入测试，所有字符都只出现一次
+        Test("abcdefg", 'a');
+
+        // 鲁棒性测试，输入null
+        Test(null, '\0');
+    }
+
+    // ====================测试代码====================
+    public static void Test(string testName, P244NonDupCharInStream chars, char expected)
+    {
+        if (testName != null)
+            Console.Write($"{testName} begins: ");
+
+        if (chars.FirstAppearingOnce() == expected)
+            Console.Write("Passed.\n");
+        else
+            Console.Write("FAILED.\n");
+    }
+
+    public static void P224Test02()
+    {
+        P244NonDupCharInStream chars = new();
+
+        Test("Test1", chars, '\0');
+
+        chars.Insert('g');
+        Test("Test2", chars, 'g');
+
+        chars.Insert('o');
+        Test("Test3", chars, 'g');
+
+        chars.Insert('o');
+        Test("Test4", chars, 'g');
+
+        chars.Insert('g');
+        Test("Test5", chars, '\0');
+
+        chars.Insert('l');
+        Test("Test6", chars, 'l');
+
+        chars.Insert('e');
+        Test("Test7", chars, 'l');
+    }
+
     public class P244NonDupCharInStream
     {
         private int _currentIndex = 0;
@@ -51,7 +111,7 @@ public static partial class Solution
             {
                 var charIndex = _occuCountArr[i];
 
-                if(charIndex >= 0)
+                if (charIndex >= 0)
                 {
                     if (minCharIndex == -1 || charIndex < minCharIndex)
                     {
@@ -62,7 +122,7 @@ public static partial class Solution
             }
 
             if (minCharIndex >= 0)
-            { 
+            {
                 return (char)(minChar + 'A');
             }
 
