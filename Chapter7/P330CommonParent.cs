c7de6e68e012a6cae4b89a2ad35bb9abc001041e
Shaun Chen <shang.chen@globirdenergy.com.au>
2022-09-20 10:56:16 +0800

    1. continued

---
+++ Chapter7/P330CommonParent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithm.Chapter7;
+
+public static partial class Solution
+{
+    public class MultiChildNode<T>
+    {
+        public T Value { get; set; }
+
+        public ICollection<T> Children { get; set; } = new List<T>();
+    }
+
+    // public static int? P330CommonParent(string source)
+    // {
+        
+    // }
+}
