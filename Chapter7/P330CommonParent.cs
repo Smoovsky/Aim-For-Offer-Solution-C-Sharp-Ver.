9569d92734bb88ed3a5a0a8ec4c6639ae99f9ebe
Shaun Chen <shang.chen@globirdenergy.com.au>
2022-09-26 00:14:46 +0800

    1. continued

--- Chapter7/P330CommonParent.cs
+++ Chapter7/P330CommonParent.cs
@@ -8,11 +8,53 @@ public static partial class Solution
     {
         public T Value { get; set; }
 
-        public ICollection<T> Children { get; set; } = new List<T>();
+        public ICollection<MultiChildNode<T>> Children { get; set; } = new List<MultiChildNode<T>>();
     }
 
     // public static int? P330CommonParent(string source)
     // {
-        
+
     // }
+
+    public static ICollection<MultiChildNode<T>> GetPath<T>(
+        MultiChildNode<T> current,
+        MultiChildNode<T> node,
+        ref bool found,
+        List<MultiChildNode<T>> path = null)
+    {
+        path ??= new List<MultiChildNode<T>>();
+
+        path.Add(current);
+
+        if (current == node)
+        {
+            found = true;
+
+            return path;
+        }
+
+        if (current.Children?.Any() != true)
+        {
+            path.RemoveAt(path.Count - 1);
+
+            return path;
+        }
+
+        foreach (var child in current.Children)
+        {
+            GetPath(child, node, ref found, path);
+
+            if (found)
+            {
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            path.RemoveAt(path.Count - 1);
+        }
+
+        return path;
+    }
 }
